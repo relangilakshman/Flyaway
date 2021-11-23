a9079ee4f7b03660d2fd7c039052fe060e92aa83
Lakshman Kumar Relangi <lakshman.relangi@COMVIVA.COM>
2021-11-23 14:58:02 +0530

    first commit

---
+++ src/test/java/com/flyaway/FlyawayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flyaway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyawayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
